70d25f8916d2f5080632ef61a6b8a30e3df08bdc
nicklllson <nikita.vasia2015@gmail.com>
2024-07-18 09:23:46 +0500

    finish

--- src/components/Flowers.tsx
+++ src/components/Flowers.tsx
@@ -13,7 +13,7 @@ export const Flowers = () => {
       <div className="absolute top-[54rem] -left-[24rem] scale-[2.4] pointer-events-none z-10 2xl:-left-[28rem] xl:scale-[2] xl:-left-[36rem] md:scale-[0.8] md:top-[3rem] md:-left-[31rem] sm:-left-[33rem]">
         <Image src="/images/flowers/3.webp" priority alt="Цветок" width={752} height={1935}></Image>
       </div>
-      <div className="absolute bottom-[32rem] -left-[15rem] scale-[1.4] pointer-events-none z-10 2xl:-left-[20rem] xl:scale-[1.2] md:scale-[0.6] md:top-[45rem] md:-left-[22rem]">
+      <div className="absolute bottom-[32rem] -left-[15rem] scale-[1.4] pointer-events-none z-10 2xl:-left-[20rem] xl:scale-[1.2] md:scale-[0.6] md:top-[35rem] md:-left-[22rem]">
         <Image src="/images/flowers/4.webp" alt="Цветок" width={960} height={984}></Image>
       </div>
       <div className="absolute -bottom-[13rem] left-0 scale-[1] pointer-events-none z-10 2xl:scale-[0.7] 2xl:-bottom-[8rem] xl:-bottom-[7rem] xl:-left-[4rem] md:scale-[0.55] md:-bottom-[8.2rem] md:-left-[16rem] sm:-bottom-[7.6rem]">
@@ -22,7 +22,7 @@ export const Flowers = () => {
       <div className="absolute -top-[32rem] -right-[26rem] scale-[1.2] pointer-events-none md:-top-[22rem] xl:scale-100 md:-right-[25rem] md:scale-[0.40]">
         <Image src="/images/flowers/6.webp" alt="Цветок" width={1088} height={1130}></Image>
       </div>
-      <div className="absolute top-[43rem] -right-[18rem] scale-[2] pointer-events-none z-10 2xl:-right-[25rem] xl:scale-[1.7] md:scale-[0.6] md:top-[41rem] md:-right-[20rem]">
+      <div className="absolute top-[43rem] -right-[18rem] scale-[2] pointer-events-none z-10 2xl:-right-[25rem] xl:scale-[1.7] md:scale-[0.6] md:top-[29rem] md:-right-[20rem]">
         <Image src="/images/flowers/7.webp" alt="Цветок" width={850} height={2186}></Image>
       </div>
       <div className="absolute -bottom-[2rem] -right-[28rem] pointer-events-none scale-[1.45] z-10 2xl:scale-[1.2] xl:scale-[0.7] md:scale-[0.5] md:-top-[26rem] md:-right-[34rem]">
--- src/components/Listing.tsx
+++ src/components/Listing.tsx
@@ -18,27 +18,14 @@ export const Listing = () => {
             <Image className="md:w-[24px] h-auto" width={33} height={33} src="/images/svg/star.svg" alt="Иконка зведочки" />
             <p>Адрес и время венчания:</p>
           </div>
-          <div className="flex flex-col">
+          <div className="flex flex-row gap-[20px] md:flex-col md:gap-0">
             <span className="list-descritption">ул. Красноармейская <span className={cinzel.className}>57</span></span>
-            <div className="flex flex-col items-end -mt-[31px] md:-mt-[0] md:items-start md:ml-[32px] list-children">
+            <div className="flex flex-col items-end mt-[16px] md:mt-[0] md:items-start md:ml-[32px] list-children">
               <span className="list-children-item">Сбор гостей в <span className={cinzel.className}>{mainInfo.guestsTime}</span></span>
               <span className="list-children-item">
                 Начало в <span className={cinzel.className}>{mainInfo.startOfWedding}</span>
               </span>
             </div>
-          </div>
-        </li>
-
-        <li className="list-item">
-          <div className="list-title">
-            <Image className="md:w-[24px] h-auto" width={33} height={33} src="/images/svg/star.svg" alt="Иконка зведочки" />
-            <p>Банкет по адресу:</p>
-          </div>
-          <span className="list-descritption">
-            ул.Темирязева <span className={cinzel.className}>28</span>, ресторан “Sicillia”
-          </span>
-          <div className="flex flex-col items-end list-children md:items-start ml-[32px]">
-            <span className="list-children-item">Начало в <span className={cinzel.className}>{mainInfo.startOfBanket}</span></span>
           </div>
         </li>
 
